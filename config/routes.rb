9b387dc5c5c48b6e2cc68cd014db847e2cc0063a
Gurshan Sekhon <gsekhon41@rrc.ca>
2026-03-05 14:47:19 -0600

    Initial Rails CRM setup with Customer model and ActiveAdmin

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+
+  root 'customers#index'
+  get '/customers/missing_email', to: 'customers#missing_email', as: :missing_email
+  get '/customers/alphabetized',  to: 'customers#alphabetized',  as: :alphabetized
+end
